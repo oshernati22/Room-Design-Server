2f69eff35e910f4df19e1d143c2bcf950077db04
Osher <oshernati22@gmail.com>
2021-12-09 16:48:43 +0200

    change port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const resolvers = require("./furniture/resolvers");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 async function startServer() {
   const app = express();
